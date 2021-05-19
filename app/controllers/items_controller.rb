d693790d1bccbf4f2c2dadb3779aef533ab10035
y-kengo <yamaken0904@gmail.com>
2021-05-19 19:58:13 +0900

    商品情報編集機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,8 +18,22 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @items = Item.find(params[:id])
+    @item = Item.find(params[:id])
   end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to root_path
+    else
+       render :edit
+    end
+  end
+  
 
   private
   def item_params
